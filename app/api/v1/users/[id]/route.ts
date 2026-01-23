6b273b1cd3bb4d235abb542b72c3c0cb62815145
wajahatfaryadali <wajahatfaryadali@gmail.com>
2026-01-24 00:17:47 +0500

    get individual user route added

---
+++ app/api/v1/users/[id]/route.ts
@@ -0,0 +1,45 @@
+import prisma from "@/db/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  try {
+    const { id } = await params;
+
+    const user = await prisma.user.findUnique({
+      where: { id: id },
+      omit: {
+        password: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "User Not Found!",
+          data: null,
+          message: "not found",
+          success: false,
+        },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(
+      { data: user, message: "user by id", success: true },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log("error getting all users ******* ", error);
+    return NextResponse.json(
+      {
+        error: "Internal Server Error",
+        message: "Internal Server Error",
+        success: false,
+      },
+      { status: 500 },
+    );
+  }
+};
